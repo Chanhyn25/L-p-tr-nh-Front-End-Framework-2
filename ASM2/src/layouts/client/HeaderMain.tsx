63119cb1c58e87374dd63dfa1261aa6266b53507
usbrit91 <usbrit914@gmail.com>
2024-07-30 21:18:43 +0700

    update: layout client

--- ASM2/src/layouts/client/HeaderMain.tsx
+++ ASM2/src/layouts/client/HeaderMain.tsx
@@ -3,36 +3,37 @@ import { Link } from "react-router-dom";
 
 const MainHeader: React.FC = () => {
   return (
-    <header className="bg-blue-600 text-white p-4 shadow-md">
+    <header className="bg-black text-white p-8 ">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">My App</h1>
+        <h1 className="text-2xl font-bold">Royal Sofas</h1>
         <nav>
-          <ul className="flex space-x-4">
+          <ul className="flex space-x-10">
             <li>
-              <Link to="/" className="hover:text-gray-300">
-                Home
+              <Link to="/" className="hover:text-gray-300 flex items-center ">
+              <ion-icon name="home-outline" style={{ fontSize: '24px', marginRight:'10px'}}></ion-icon> 
+              Home
               </Link>
             </li>
             <li>
-              <Link to="/categories" className="hover:text-gray-300">
-                Categories
+              <Link to="/categories" className="hover:text-gray-300 flex items-center ">
+              <ion-icon name="file-tray-full-outline" style={{ fontSize: '24px', marginRight:'10px' }}></ion-icon> Categories
               </Link>
             </li>
             <li>
-              <Link to="/cart" className="hover:text-gray-300">
-                Cart
+              <Link to="/cart" className="hover:text-gray-300 flex items-center ">
+              <ion-icon name="cart-outline" style={{ fontSize: '24px' , marginRight:'10px'}}></ion-icon> Cart
               </Link>
             </li>
             <li>
-              <Link to="/users" className="hover:text-gray-300">
-                Users
+              <Link to="/users" className="hover:text-gray-300 flex items-center ">
+              <ion-icon name="person-circle-outline" style={{ fontSize: '24px' , marginRight:'10px'}} ></ion-icon> 
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link to="/login" className="hover:text-gray-300">
                 Login
               </Link>
-            </li>
+            </li> */}
           </ul>
         </nav>
       </div>
--- ASM2/src/pages/client/CategoriesPage.tsx
+++ ASM2/src/pages/client/CategoriesPage.tsx
@@ -6,17 +6,17 @@ const CategoriesPage: React.FC = () => {
       <h1 className="text-3xl font-bold mb-4">Categories</h1>
       <ul className="space-y-2">
         <li className="border-b pb-2">
-          <a href="#" className="text-blue-500 hover:underline">
+          <a href="#" className="text-black hover:underline">
             Category 1
           </a>
         </li>
         <li className="border-b pb-2">
-          <a href="#" className="text-blue-500 hover:underline">
+          <a href="#" className="text-black hover:underline">
             Category 2
           </a>
         </li>
         <li>
-          <a href="#" className="text-blue-500 hover:underline">
+          <a href="#" className="text-black hover:underline">
             Category 3
           </a>
         </li>
